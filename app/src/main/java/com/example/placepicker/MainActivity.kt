84e4e294db5b31891b7db45726c0837c001a0075
newteen <niltinhosupriano@gmail.com>
2021-08-25 22:42:31 -0300

    implement component at main activity

--- app/src/main/java/com/example/placepicker/MainActivity.kt
+++ app/src/main/java/com/example/placepicker/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.placepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        subscribeLiveData()
+    }
+
+    private fun subscribeLiveData() {
+        main_place_picker.address.observe(this,
+            { main_cep_txt.text = it })
+
+        main_place_picker.isMapClicked.observe(this,
+            { card_view_cep_layout.isVisible = !it })
     }
 }
